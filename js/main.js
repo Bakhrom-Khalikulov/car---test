bafadef6bae64491420e0a1f0fb86691ca6fc49d
Bob Brandon <bakhromkhalikulov@gmail.com>
2023-05-15 19:19:44 -0700

    car section done

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var elCarsList = document.querySelector(".js-cars__list");
+var elCarTemplate = document.querySelector(".js-car__template").content;
+var newCarsFragment = document.createDocumentFragment();
+
+function getCars(_cars, _node){
+    _cars.forEach(function(car){
+        var newCloneTemplate = elCarTemplate.cloneNode(true);
+
+        newCloneTemplate.querySelector(".js-car__img").src = car.image;
+        newCloneTemplate.querySelector(".js-car__title").textContent = car.title;
+        newCloneTemplate.querySelector(".js-car__class").textContent = car.class;
+        newCloneTemplate.querySelector(".js-car__year").textContent = car.start_production;
+
+        newCarsFragment.appendChild(newCloneTemplate);
+
+    })
+
+    _node.appendChild(newCarsFragment);
+}
+
+getCars(cars, elCarsList);
