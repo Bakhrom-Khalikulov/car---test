038fcee1cbf412dc19a55504277e451a1f7e45dc
Bob Brandon <bakhromkhalikulov@gmail.com>
2023-05-15 19:22:21 -0700

    car section updated

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var elCarTemplate = document.querySelector(".js-car__template").content;
 var newCarsFragment = document.createDocumentFragment();
 
 function getCars(_cars, _node){
-    _cars.forEach(function(car){
+    _cars.splice(0, 100).forEach(function(car){
         var newCloneTemplate = elCarTemplate.cloneNode(true);
 
         newCloneTemplate.querySelector(".js-car__img").src = car.image;
